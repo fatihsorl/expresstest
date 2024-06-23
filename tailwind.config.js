b298957302ad13e79ea3f0ec7c6e53e26a828c3a
fatihsorll <fatih.soral@gmail.com>
2024-06-23 20:26:25 +0300

    some developments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1208px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1636px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '2080px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 };
